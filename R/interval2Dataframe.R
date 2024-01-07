f754a4e258f68e7e057e0f4fc3eb59432ce442d6
criss-bassu <cbaronsuarez@gmail.com>
2024-01-07 22:03:11 +0100

    Generalizar operaciones a listas

--- R/interval2Dataframe.R
+++ R/interval2Dataframe.R
@@ -4,8 +4,24 @@
 #' @return
 #' @export
 #' @examples
-interval2Dataframe=function(intervals){
+interval2Dataframe=function(intervals, .id=c()){
+  #Por si hay que añadir columnas con nombres
+      if(length(.id)>0) {
+        thisId=.id[1]
+        .id=.id[-1]
+      } else {thisId=c()
+             .id=c()}
+
+  #Para el caso de una lista
+   if (is.list(intervals)){
+     result=map(intervals, interval2Dataframe,.id=.id)
+     if(length(thisId)==1) result=bind_rows(result,.id=thisId)
+     return (result)
+  }
+  
+  # Para el caso del tipo base
   result=intervals %>% enframe() %>% unnest(value) %>% mutate(start=int_start(value),end=int_end(value)) %>% select(-value) 
-  if(is.null(names(intervals))) return(result %>% select(-name))  
+  if(is.null(names(intervals))) return(result %>% select(-name)) 
   result %>% mutate(name=str_replace(name,"\\.\\.[0-9]+$",""))
+  #result
 }
