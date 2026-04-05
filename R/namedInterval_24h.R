d7f9e10c847a5095a26113ede5a4e889af1e4d25
criss-bassu <cbaronsuarez@gmail.com>
2026-04-05 18:10:13 +0200

    Better namedInterval_24h

--- R/namedInterval_24h.R
+++ R/namedInterval_24h.R
@@ -11,46 +11,33 @@
 #' @return A named (or unnamed) lubridate interval vector containing the daily windows.
 #' @export
 measureInterval_24h<-function(df,first=TRUE,last=TRUE,offsetLabels=dhours(0),starts=dhours(0),duration=period(1,"days"),.isOn=NULL,withName=TRUE){
-  if(!is.null(.isOn)) {
-    start=int_start(.isOn[1])
-    end=int_end(.isOn[1])
-  } else {
-  start=first(df$timestamp)
-  end=last(df$timestamp)
+    if (!is.null(.isOn)) {
+      start = int_start(.isOn[1])
+      end = int_end(.isOn[1])
+    }  else {
+      start = first(df$timestamp)
+      end = last(df$timestamp)
+    }
+    inicio <- floor_date(start, unit = "day")
+    fin <- ceiling_date(end, unit = "day")+dhours(1)
+    fechas_medianoche <- seq(from = inicio, to = fin, by = "24 hours")
+    from = (fechas_medianoche) + starts
+    if (!first)
+      from = from[-1]
+    if (!last)
+      from = from[-length(from)]
+    numDias = length(from) - 1
+    empieza = from[1:numDias]
+    if (duration == period(1, "days")) {
+      termina = from[2:length(from)]
+    }  else {
+      termina = empieza + duration
+    }
+    if (withName) {
+      interval(empieza, termina) %>% set_names(format(from[1:numDias] +
+                                                        offsetLabels, "%Y-%m-%d"))
+    }  else {
+      interval(empieza, termina)
+    }
   }
   
-  
-  # problematic with summer time... Probably need another When to deal with this
-  #from=(start-dhours(hour(start))-dminutes(minute(start))-dseconds(second(start)))+starts+ddays(0:as.integer(as_date(end)-as_date(start)))
-  #start=ymd_hms("2019-10-27 00:00:00",tz="Europe/Madrid")
-  #end=ymd_hms("2019-10-30 00:00:00",tz="Europe/Madrid")
-  inicio <- floor_date(start, unit = "day")
-  fin <- ceiling_date(end, unit = "day")
-  
-  fechas_medianoche <- seq(from = inicio, to = fin, by = "day")
-  redondeoSup=ceiling_date(fechas_medianoche, unit = "day") %>% unique()
-  redondeoInf=floor_date(fechas_medianoche, unit = "day") %>% unique()
-  if(length(redondeoSup)<length(redondeoInf)) fechas_medianoche=redondeoInf
-  if(length(redondeoInf)<length(redondeoSup)) fechas_medianoche=redondeoSup
-  
-  
-  from=(fechas_medianoche)+starts
-
-
-  
-  if(!first) from=from[-1]
-  if(!last) from=from[- length(from)]
-  numDias=length(from)-1
-  
-  empieza=from[1:numDias]
-  if(duration==period(1,"days")) {termina=from[2:length(from)] 
-  } else {termina = empieza+duration}
-  
-  
-  if (withName){
-    interval(empieza,termina) %>%
-      set_names(format(from[1:numDias]+offsetLabels, "%Y-%m-%d"))
-} else {
-  interval(empieza,termina)
-}
-}
